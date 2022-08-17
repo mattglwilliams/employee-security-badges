c363d6065a5b55283368306756a1c058b8fd6219
Matthew Williams <mattwilliamsdev@gmail.com>
2022-08-17 14:24:36 +0100

    Separated the code in Main to separate methods

--- CatWorx.BadgeMaker/Program.cs
+++ CatWorx.BadgeMaker/Program.cs
@@ -3,21 +3,38 @@ using System.Collections.Generic;
 
 namespace CatWorx.BadgeMaker
 {
-  class Program
-  {
-    static void Main(string[] args)
+    class Program
     {
-      List<string> employees = new List<string>() { "adam", "amy" };
-      employees.Add("barbara");
-      employees.Add("billy");
-      Console.WriteLine("Please enter a name: ");
-      // Get a name from the console and assign it to a variable
-      string input = Console.ReadLine();
-      employees.Add(input);
-      for (int i = 0; i < employees.Count; i++) 
-      {
-        Console.WriteLine(employees[i]);
-      }
+        static List<string> GetEmployees()
+        {
+            List<string> employees = new List<string>();
+            while (true)
+            {
+                Console.WriteLine("Please enter a name: (leave empty to exit): ");
+
+                string input = Console.ReadLine();
+
+                if (input == "")
+                {
+                    break;
+                }
+                employees.Add(input);
+            }
+            return employees;
+        }
+
+        static void PrintEmployees(List<string> employees)
+        {
+            for (int i = 0; i < employees.Count; i++)
+            {
+                Console.WriteLine(employees[i]);
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            List<string> employees = GetEmployees();
+            PrintEmployees(employees);
+        }
     }
-  }
 }
