0a618ad1c172d75796d3abe8e76f68a2b90af210
Matthew Williams <mattwilliamsdev@gmail.com>
2022-08-18 10:05:46 +0100

    Added logic to add for remaining Employee properties

--- CatWorx.BadgeMaker/Program.cs
+++ CatWorx.BadgeMaker/Program.cs
@@ -18,7 +18,13 @@ namespace CatWorx.BadgeMaker
                 {
                     break;
                 }
-                Employee currentEmployee = new Employee(input);
+                Console.Write("Enter last name: ");
+                string lastName = Console.ReadLine();
+                Console.Write("Enter ID: ");
+                int id = Console.ReadLine();
+                Console.Write("Enter Photo URL:");
+                string photoUrl = Console.ReadLine();
+                Employee currentEmployee = new Employee(firstName, lastName, id, photoUrl);
                 employees.Add(currentEmployee);
             }
             return employees;
