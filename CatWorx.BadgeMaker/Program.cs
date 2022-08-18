01895a92cbdb62ea7189a7e667c96cb951eecd7d
Matthew Williams <mattwilliamsdev@gmail.com>
2022-08-18 10:11:12 +0100

    Changed some console prompts

--- CatWorx.BadgeMaker/Program.cs
+++ CatWorx.BadgeMaker/Program.cs
@@ -10,7 +10,7 @@ namespace CatWorx.BadgeMaker
             List<Employee> employees = new List<Employee>();
             while (true)
             {
-                Console.WriteLine("Please enter a name: (leave empty to exit): ");
+                Console.WriteLine("Please enter a first name: (leave empty to exit): ");
 
                 string firstName = Console.ReadLine();
 
@@ -22,7 +22,7 @@ namespace CatWorx.BadgeMaker
                 string lastName = Console.ReadLine();
                 Console.Write("Enter ID: ");
                 int id = Int32.Parse(Console.ReadLine());
-                Console.Write("Enter Photo URL:");
+                Console.Write("Enter Photo URL: ");
                 string photoUrl = Console.ReadLine();
                 Employee currentEmployee = new Employee(firstName, lastName, id, photoUrl);
                 employees.Add(currentEmployee);
